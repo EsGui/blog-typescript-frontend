827ce02a8a926368a6c0e3ed5ec91f1830b99ef1
EsGui <guilhermedagama2021100@gmail.com>
2024-05-08 12:35:04 -0300

    adicionando icones no header

--- src/components/home_components/HomeHeader.tsx
+++ src/components/home_components/HomeHeader.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image'
+import styles from '../../styles/home_styles/HomeHeader.module.css';
+import ComicBook from '../../../public/icons/comic-book.png';
+import User from '../../../public/icons/user.png'
+
 export default function HomeHeader() {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className={styles.HomeHeaderContainer}>
+            <div>
+                <Image width={80} height={80} src={ComicBook} alt="Image book" />
+            </div>
+            <div>
+                <Image width={50} height={50} src={User} alt="User" />
+            </div>
         </div>        
     )
 }
