6589deff85e8c16d76420baa22593854bf0888a6
EsGui <guilhermedagama2021100@gmail.com>
2024-05-13 23:13:00 -0300

    listando blogs no body da home

--- src/components/home_components/HomeBody.tsx
+++ src/components/home_components/HomeBody.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
-import style from '../../styles/home_styles/HomeBody.module.css';
+import styles from '../../styles/home_styles/HomeBody.module.css';
 import { useEffect } from 'react';
 import { fetchAllBlogs } from '@/lib/features/blogs/blogsSlice';
 import { fetchPersistLogin } from '@/lib/features/blogs/usersSlice';
+import Image from 'next/image';
 
 export default function HomeBody() {
     const userLogged = useAppSelector((state) => state.users.user);
@@ -21,9 +22,20 @@ export default function HomeBody() {
 
     console.log(blogs);
 
+    const openBlog = () => {
+
+    }
+
     return (
-        <div className={style.HomeBodyContainer}>
-            <h1>Testando</h1>
+        <div className={styles.HomeBodyContainer}>
+            {
+                blogs.map(({ title, coverImage, content }: any, index) => (
+                    <div className={ styles.Blog } onClick={openBlog} key={index}>
+                        <img width="100%" height="100%" src={ coverImage } alt="cover image" />
+                        <p>{ title }</p>
+                    </div>
+                ))
+            }
         </div>
     )
 }
